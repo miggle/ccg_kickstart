bf7c558ab8fdfeba94a95ee225370bba6358b11a
Andre Sutherland <andrelsf95@gmail.com>
2016-10-20 10:47:13 +0100

    Removed all non-profile files that were the original code-base before.

--- docroot/profiles/custom/ccg_kickstart/modules/ccg_features/ccg_choice_widget/js/nhs_global_widget.js
+++
@@ -1,8 +0,0 @@
-(function($, Drupal) {
-
-    Drupal.behaviors.nhsGlobalChoicesWidget = {
-        attach: function (context, settings) {
-            $('#nhs-global-choices-widget', context).fsNHSWidget([1, 2, 3, 4]);
-        }
-    };
-})(jQuery, Drupal);
